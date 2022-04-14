af81a5a15c7110e86ca3fb6b7259fbfb17eb911f
박종훈 <m05214@naver.com>
2022-04-14 18:02:24 +0900

    요구사항 1에 대한 테스트코드 작성
    assertj 의존성 다운로드 및 String 학습 요구사항1의 테스트 코드작성

--- src/test/java/string/StringTestReq1.java
+++ src/test/java/string/StringTestReq1.java
@@ -1,7 +1,14 @@
 package string;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class StringTestReq1 {
 
+	// Given
+	final String splitSampleData = "1,2" ;
 	/**
 	 * 요구사항 1
 	 * "1,2"을 , 로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
@@ -11,4 +18,34 @@ public class StringTestReq1 {
 	 * 배열로 반환하는 값의 경우 assertj의 contains()를 활용해 반환 값이 맞는지 검증한다.
 	 * 배열로 반환하는 값의 경우 assertj의 containsExactly()를 활용해 반환 값이 맞는지 검증한다.
 	 */
+
+	@DisplayName("String 클래스 중 split 메소드 학습 - 결과순서 X")
+	@Test
+	public void reqCase1(){
+		// When
+		final String[] split = splitSampleData.split(",");
+
+		// Then
+		assertThat(split).contains("1", "2");
+	}
+
+	@DisplayName("String 클래스 중 split 메소드 학습 - 결과순서 X, 하나만 있어도")
+	@Test
+	public void reqCase2(){
+		// When
+		final String[] split = splitSampleData.split(",");
+
+		// Then
+		assertThat(split).contains("1");
+	}
+
+	@DisplayName("String 클래스 중 split 메소드 학습 - 결과순서 O")
+	@Test
+	public void reqCase3(){
+		// When
+		final String[] split = splitSampleData.split(",");
+
+		// Then
+		assertThat(split).containsExactly("1", "2");
+	}
 }
